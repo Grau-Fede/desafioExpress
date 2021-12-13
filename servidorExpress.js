572788ba20d287d22450e83194c66e88fa45b3d6
Grau-Fede <56159170+Grau-Fede@users.noreply.github.com>
2021-12-13 17:05:13 -0300

    Update servidorExpress.js

--- servidorExpress.js
+++ servidorExpress.js
@@ -1,6 +1,8 @@
+import {contenedor} from "./ManArchivos";
+
 const express = require ('express');
 const path = require ('path');
-
+const contenido = new contenedor ("productos.txt");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
@@ -13,4 +15,24 @@ server.on("error", error => console.log (`Erorr en servidor ${error}`))
 
 app.get('/', (req, res)=>{
     res.sendFile(path.resolve(__dirname,'index.html'))
-})
+    
+}
+)
+app.get('/productos', (req, res)=>{
+   let arrayProductos = obtenerProductos();
+   res.send(arrayProductos);
+    
+}
+)
+app.get('/productoRandom', (req, res)=>{
+    res.send(numeroRandom);
+    
+}
+)
+let obtenerProductos = () => {
+    const lista = contenido.getAll();
+    return lista;
+}
+let numeroRandom = () => {
+    const numR= Math.random() * (contenedor.length() - 0) +0 ;
+}
